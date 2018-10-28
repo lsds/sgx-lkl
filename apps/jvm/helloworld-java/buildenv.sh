c438096fa839bdac6f48f7ba414a18685d09044a
Christian Priebe <cp3213@ic.ac.uk>
2018-10-28 21:49:23 +0000

    No need to add Alpine APK community repository in buildenv.sh as it's already present in /etc/apk/repositories.

--- apps/jvm/helloworld-java/buildenv.sh
+++ apps/jvm/helloworld-java/buildenv.sh
@@ -2,6 +2,5 @@
 
 set -ex
 
-echo "http://dl-cdn.alpinelinux.org/alpine/v3.6/community" >> /etc/apk/repositories
 apk update
 apk add iputils iproute2 unzip libstdc++
--- apps/miniroot/buildenv.sh
+++ apps/miniroot/buildenv.sh
@@ -5,7 +5,6 @@ set -ex
 PATH=/usr/sbin:/sbin:/usr/bin:/bin
 
 cd /home
-echo "http://dl-cdn.alpinelinux.org/alpine/v3.6/community" >> /etc/apk/repositories
 apk update
 apk add iputils iproute2 unzip libstdc++
 apk add redis
