9deee924ae375281dd4b855a8a784b17d3ccf7f2
Maik Riechert <marieche@microsoft.com>
2020-04-29 11:58:05 +0100

    add ci

---
+++ .azure-pipelines/scripts/install_prerequisites.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+sudo apt-get update
+sudo apt-get install -y \
+    build-essential \
+    curl wget rsync pv \
+    make gcc g++ bc python xutils-dev flex bison autogen libgcrypt20-dev libjson-c-dev \
+    autopoint pkgconf autoconf libtool libcurl4-openssl-dev libprotobuf-dev libprotobuf-c-dev protobuf-compiler protobuf-c-compiler libssl-dev \
+    ninja-build ansible linux-headers-$(uname -r) \
+    docker.io python3-venv unzip dkms openjdk-8-jdk-headless
