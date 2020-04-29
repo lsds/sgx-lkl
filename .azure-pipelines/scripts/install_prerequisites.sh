0b62003926a107c4bc064a94b7cc67c104edd009
Maik Riechert <marieche@microsoft.com>
2020-04-29 17:09:44 +0100

    add missing dependencies

--- .azure-pipelines/scripts/install_prerequisites.sh
+++ .azure-pipelines/scripts/install_prerequisites.sh
@@ -8,4 +8,4 @@ sudo apt-get install -y \
     make gcc g++ bc python xutils-dev flex bison autogen libgcrypt20-dev libjson-c-dev \
     autopoint pkgconf autoconf libtool libcurl4-openssl-dev libprotobuf-dev libprotobuf-c-dev protobuf-compiler protobuf-c-compiler libssl-dev \
     ninja-build ansible linux-headers-$(uname -r) \
-    docker.io python3-venv unzip dkms openjdk-8-jdk-headless
+    docker.io python3-venv unzip dkms debhelper apt-utils openjdk-8-jdk-headless
