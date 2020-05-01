645bf3bc8d7e3eaeb739aedc944aee3716b8a3f9
Sean T. Allen <sean@seantallen.com>
2020-05-01 16:25:38 +0000

    Correct syscall override for SYS_futex

--- src/lkl/syscall-overrides.c
+++ src/lkl/syscall-overrides.c
@@ -1,7 +1,7 @@
-#include <lkl/asm/host_ops.h>
 #include "lkl/syscall-overrides-futex.h"
+#include "lkl/posix-host.h"
 
 void register_lkl_syscall_overrides()
 {
-    lkl_replace_syscall(202, (void *)syscall_SYS_futex_override);
+    lkl_replace_syscall(__lkl__NR_futex, (void *)syscall_SYS_futex_override);
 }
