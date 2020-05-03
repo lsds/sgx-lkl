96f813d15f5b7f84d2e32357d3879091af183617
Maik Riechert <marieche@microsoft.com>
2020-05-03 18:26:47 +0100

    remove .lock files in .git before submodule update

--- .azure-pipelines/scripts/checkout_submodules.sh
+++ .azure-pipelines/scripts/checkout_submodules.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -ex
+
 mkdir -p ~/.ssh > /dev/null
 chmod 700 ~/.ssh
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+find .git -type f -name '*.lock' -exec rm -v {} +
 git reset --hard
 git submodule sync
 git submodule update --init --force --recursive
