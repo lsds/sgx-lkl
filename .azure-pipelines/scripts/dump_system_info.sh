0d33d66cca1623ff893e74fb85fb2196075bb2b1
Maik Riechert <marieche@microsoft.com>
2020-05-03 18:41:40 +0100

    dump system info in CI jobs

---
+++ .azure-pipelines/scripts/dump_system_info.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+echo "CPU info:"
+lscpu
+echo
+echo "Available memory:"
+free -h
+echo
+echo "Available disk space:"
+df -h
+echo
+echo "Distribution:"
+lsb_release -ds
+echo
+echo "SGX kernel module:"
+modinfo intel_sgx
+echo
+echo "Running SGX-LKL instances:"
+ps -aux | grep sgx-lkl-run-oe | grep -v grep || echo "none"
+echo
+echo "Environment variables:"
+printenv | sort
