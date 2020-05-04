7a14728343a5952ef7a64e76ad1c7d2f6cded052
Sean T. Allen <sean@seantallen.com>
2020-05-04 13:34:20 +0000

    Remove unused header declaration

--- src/include/enclave/enclave_mem.h
+++ src/include/enclave/enclave_mem.h
@@ -50,14 +50,6 @@ int syscall_SYS_enclave_futex(
     int* uaddr2,
     int val3);
 
-long syscall_SYS_futex_override(
-    int* uaddr,
-    int op,
-    int val,
-    const struct timespec* timeout,
-    int* uaddr2,
-    int val3);
-
 void* syscall_SYS_mmap(
     void* addr,
     size_t length,
