738b3c551b55089a1351a6d97507803764c8c1c0
Maik Riechert <maik.riechert@microsoft.com>
2020-05-14 11:27:31 +0000

    rewrite debian packaging script based on lddtree

--- .azure-pipelines/scripts/install_prerequisites.sh
+++ .azure-pipelines/scripts/install_prerequisites.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y \
     make gcc g++ bc python xutils-dev flex bison autogen libgcrypt20-dev libjson-c-dev \
     autopoint pkgconf autoconf libtool libcurl4-openssl-dev libprotobuf-dev libprotobuf-c-dev protobuf-compiler protobuf-c-compiler libssl-dev \
     ninja-build ansible linux-headers-$(uname -r) \
-    docker.io python3-venv unzip dkms debhelper apt-utils openjdk-8-jdk-headless \
+    docker.io python3-venv unzip dkms debhelper apt-utils pax-utils openjdk-8-jdk-headless \
     expect
 
 # Allow to run Docker without sudo
