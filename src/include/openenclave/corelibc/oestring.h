d0ab39cdaaf985fc52369b347601df4f1d882f72
Sean T. Allen <sean@seantallen.com>
2020-05-27 14:43:28 +0000

    Remove libc dependencies from sgxlkl_app_config
    Removes libc dependencies except for memset which would be emitted
    by the compiler.
    
    This is part of the work for #151

--- src/include/openenclave/corelibc/oestring.h
+++ src/include/openenclave/corelibc/oestring.h
@@ -1,6 +1,8 @@
 #ifndef __OE_STRING_INCLUDED__
 #define __OE_STRING_INCLUDED__
 
+int oe_strcmp(const char* s1, const char* s2);
 char *oe_strdup(const char *s);
+size_t oe_strlen(const char* s);
 
 #endif
