db9409132b87bb63985a739d6a5c18146597bd6e
Maik Riechert <maik.riechert@microsoft.com>
2020-06-01 09:54:19 +0000

    remove .sh files from sample

--- samples/languages/python/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PATH="../../../tools:$PATH"
-
-sgx-lkl-disk create --docker=Dockerfile --size=300M disk.img
-sgx-lkl-cfg create --disk disk.img
--- samples/languages/python/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PATH="../../../build:$PATH"
-
-sgx-lkl-run-oe --host-config=host-config.json --app-config=app-config.json $@
