6016fbc9eabda2799ac914c8278e37d2e2f385b2
Maik Riechert <maik.riechert@microsoft.com>
2020-06-03 11:12:24 +0100

    Re-work Python sample as template for all samples (#333)

--- samples/languages/python/app/python-helloworld.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-print("Confidential Computing using Intel SGX in Python with NumPy... ")
-
-print(np.arange(10000).reshape(100,100))
---
+++ samples/languages/python/src/python-helloworld.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+print("Confidential Computing using SGX-LKL in Python with NumPy... ")
+
+print(np.arange(10000).reshape(100,100))
