ebb39ba599937fc987be3619b6a3e8b2284fede4
Xuejun Yang <xuejya@microsoft.com>
2020-06-09 15:36:15 -0700

    Handle fstat and newfstatat compatibility in lkl (#381)
    * Handle fstat and newfstatat compatibility in lkl
    
    Fix #150
    
    * Address review comments
    
    * Fix doc comments
    
    * Update submodue sgx-lkl-musl

--- src/lkl/syscall-overrides.c
+++ src/lkl/syscall-overrides.c
@@ -1,6 +1,8 @@
-#include "lkl/syscall-overrides-futex.h"
+#include <sys/syscall.h>
+#include "lkl/syscall-overrides-fstat.h"
 #include "lkl/posix-host.h"
 
 void register_lkl_syscall_overrides()
 {
+    syscall_register_fstat_overrides();
 }
