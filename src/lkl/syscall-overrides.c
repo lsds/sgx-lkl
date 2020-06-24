36ded456f160cc272524bc8dc5b0d8b4287e7b07
Xuejun Yang <xuejya@microsoft.com>
2020-06-24 07:33:38 -0700

    Override sysinfo syscall handling (#362)
    Override syscall handling for sysinfo in LKL
    
    Fixes #178

--- src/lkl/syscall-overrides.c
+++ src/lkl/syscall-overrides.c
@@ -1,8 +1,10 @@
 #include <sys/syscall.h>
 #include "lkl/syscall-overrides-fstat.h"
+#include "lkl/syscall-overrides-sysinfo.h"
 #include "lkl/posix-host.h"
 
 void register_lkl_syscall_overrides()
 {
     syscall_register_fstat_overrides();
+    lkl_replace_syscall(__lkl__NR_sysinfo, syscall_sysinfo_override);
 }
