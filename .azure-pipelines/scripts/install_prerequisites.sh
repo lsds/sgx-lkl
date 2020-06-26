8beccd21dfac4c02100e05a1b844a1744c4cf4a2
Maik Riechert <maik.riechert@microsoft.com>
2020-06-26 20:16:19 +0100

    Add sgx-lkl-cfg test (#553)

--- .azure-pipelines/scripts/install_prerequisites.sh
+++ .azure-pipelines/scripts/install_prerequisites.sh
@@ -8,8 +8,10 @@ sudo apt-get install -y \
     make gcc g++ bc python xutils-dev flex bison autogen libgcrypt20-dev libjson-c-dev \
     autopoint pkgconf autoconf libtool libcurl4-openssl-dev libprotobuf-dev libprotobuf-c-dev protobuf-compiler protobuf-c-compiler libssl-dev \
     ninja-build ansible linux-headers-$(uname -r) \
-    python3-venv unzip dkms debhelper apt-utils pax-utils openjdk-8-jdk-headless \
+    python3 python3-pip unzip dkms debhelper apt-utils pax-utils openjdk-8-jdk-headless \
     expect
+
+sudo python3 -m pip install "jsonschema>=3"
 
 if [[ ! -x "$(command -v docker)" ]]; then
     sudo apt-get install -y docker.io
