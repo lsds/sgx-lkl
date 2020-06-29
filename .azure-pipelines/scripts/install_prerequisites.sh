831f27ccf8bb67415e1e4bb1048c1615138fafd1
Maik Riechert <maik.riechert@microsoft.com>
2020-06-29 10:12:14 +0100

    ci: add missing package python3-setuptools (#566)
    This prevented installing sdist packages.

--- .azure-pipelines/scripts/install_prerequisites.sh
+++ .azure-pipelines/scripts/install_prerequisites.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y \
     make gcc g++ bc python xutils-dev flex bison autogen libgcrypt20-dev libjson-c-dev \
     autopoint pkgconf autoconf libtool libcurl4-openssl-dev libprotobuf-dev libprotobuf-c-dev protobuf-compiler protobuf-c-compiler libssl-dev \
     ninja-build ansible "linux-headers-$(uname -r)" \
-    python3 python3-pip unzip dkms debhelper apt-utils pax-utils openjdk-8-jdk-headless \
+    python3 python3-setuptools python3-pip unzip dkms debhelper apt-utils pax-utils openjdk-8-jdk-headless \
     expect \
     shellcheck clang-format
 
