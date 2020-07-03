fb2257e36d8fcfa211f347b72fd78d4da81a01f9
Maik Riechert <maik.riechert@microsoft.com>
2020-07-03 14:17:56 +0000

    Avoid using hidden files. Prevents rm -rf * within the build folder otherwise.

--- cmake/DownloadAndExtract.cmake
+++ cmake/DownloadAndExtract.cmake
@@ -6,13 +6,13 @@ function(download_and_extract_tarball NAME URL)
 	add_custom_command(OUTPUT ${TARFILE}
 		COMMAND "wget" ARGS "-nv" "-O" "${TARFILE}" "'${URL}'"
 		COMMENT "Downloading ${NAME}")
-	set(DOTFILE ".extract-${NAME}")
-	add_custom_command(OUTPUT ${DOTFILE}
+	set(STAMPFILE "extract-${NAME}.stamp")
+	add_custom_command(OUTPUT ${STAMPFILE}
 		COMMAND ${CMAKE_COMMAND} ARGS "-E" "make_directory" "${THIRD_PARTY_DOWNLOADS_DIRECTORY}/${NAME}"
 		COMMAND "tar" "-C" "${THIRD_PARTY_DOWNLOADS_DIRECTORY}/${NAME}" "--strip" "1" "-xf" ${TARFILE}
-		COMMAND ${CMAKE_COMMAND} ARGS "-E" "touch" ${DOTFILE}
+		COMMAND ${CMAKE_COMMAND} ARGS "-E" "touch" ${STAMPFILE}
 		DEPENDS ${TARFILE}
 		COMMENT "Extracting ${NAME}")
-	add_custom_target("fetch-and-extract-${NAME}" DEPENDS ${DOTFILE} COMMENT "Fetch and extract ${NAME}")
+	add_custom_target("fetch-and-extract-${NAME}" DEPENDS ${STAMPFILE} COMMENT "Fetch and extract ${NAME}")
 	set("${NAME}_LOCATION" "${THIRD_PARTY_DOWNLOADS_DIRECTORY}/${NAME}" PARENT_SCOPE)
 endfunction(download_and_extract_tarball)
