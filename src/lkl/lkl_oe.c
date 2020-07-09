2ac110196d6003fb0279fd38b9d4062707518082
Maik Riechert <maik.riechert@microsoft.com>
2020-07-09 14:33:12 +0000

    Add doc for lkl_printf/lkl_bug.

--- src/lkl/lkl_oe.c
+++ src/lkl/lkl_oe.c
@@ -3,6 +3,12 @@
 
 #define OE_STDERR_FILENO 1
 
+// LKL would normally provide implementations for lkl_printf and lkl_bug.
+// However, these would come from liblkl-in.o which relies on a libc.
+// In SGX-LKL kernel space we do not have a regular libc available.
+// Here we provide the missing implementations using OE's core libc.
+// See lkl/tools/lkl/lib/utils.c for the original implementations.
+
 int lkl_printf(const char *fmt, ...)
 {
 	int n;
