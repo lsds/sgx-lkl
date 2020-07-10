33830731f20501dfb1c212f3f7c9304a12f293a5
Maik Riechert <maik.riechert@microsoft.com>
2020-07-10 18:25:31 +0100

    Disable shallow submodule update. (#622)

--- .azure-pipelines/scripts/checkout_submodules.sh
+++ .azure-pipelines/scripts/checkout_submodules.sh
@@ -8,7 +8,7 @@ ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 find .git -type f -name '*.lock' -exec rm -v {} +
 git reset --hard
 git submodule sync
-git submodule update --init --force --recursive --depth 1
+git submodule update --init --force --recursive
 git submodule foreach git reset --hard
 git clean -xdf
 git submodule foreach git clean -xdf
