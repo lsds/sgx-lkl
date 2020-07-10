8f1d06855996842e1489cad2cd12d4dde27cfe9f
Maik Riechert <maik.riechert@microsoft.com>
2020-07-10 18:10:41 +0000

    Build oeenclave.o with hidden libc & mbedtls. Adapt from https://github.com/lsds/sgx-lkl/blob/feature.kernel-user-split/oeenclave.mak.

---
+++ src/lkl/oe_syscall_ocall_stubs.c
@@ -0,0 +1,26 @@
+// Stubs as we currently need to link in oesyscall.
+// This will disappear in the future.
+
+void abort(void);
+
+void oe_syscall_close_ocall(){abort();}
+void oe_syscall_dup_ocall(){abort();}
+void oe_syscall_epoll_wake_ocall(){abort();}
+void oe_syscall_fcntl_ocall(){abort();}
+void oe_syscall_getegid_ocall(){abort();}
+void oe_syscall_geteuid_ocall(){abort();}
+void oe_syscall_getgid_ocall(){abort();}
+void oe_syscall_getgroups_ocall(){abort();}
+void oe_syscall_getpgid_ocall(){abort();}
+void oe_syscall_getpgrp_ocall(){abort();}
+void oe_syscall_getpid_ocall(){abort();}
+void oe_syscall_getppid_ocall(){abort();}
+void oe_syscall_getuid_ocall(){abort();}
+void oe_syscall_ioctl_ocall(){abort();}
+void oe_syscall_nanosleep_ocall(){abort();}
+void oe_syscall_poll_ocall(){abort();}
+void oe_syscall_read_ocall(){abort();}
+void oe_syscall_readv_ocall(){abort();}
+void oe_syscall_uname_ocall(){abort();}
+void oe_syscall_write_ocall(){abort();}
+void oe_syscall_writev_ocall(){abort();}
