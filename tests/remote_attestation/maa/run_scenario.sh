206123d3053f561a9e48dbc5429d7d19425f7789
hukoyu <hukoyu@microsoft.com>
2020-07-21 08:49:46 +0000

    Fixing check-ci erros

--- tests/remote_attestation/maa/run_scenario.sh
+++ tests/remote_attestation/maa/run_scenario.sh
@@ -15,11 +15,10 @@ fi
 # If openenclave is not installed, install it
 if [[ ! -d "/opt/openenclave" ]]; then
     home=$(pwd)
-    cd ~
+    cd ~ || exit 1
     sudo rm -rf openenclave/
-    openenclave_install_dir="/opt/openenclave/"
     git clone --recursive -b feature/sgx-lkl-support https://github.com/openenclave/openenclave.git
-    cd openenclave
+    cd openenclave || exit 1
 
     sed -i '/add_subdirectory(tests)/d' CMakeLists.txt
 
@@ -27,24 +26,25 @@ if [[ ! -d "/opt/openenclave" ]]; then
     sudo ansible-playbook scripts/ansible/oe-contributors-acc-setup-no-driver.yml
 
     mkdir -p build
-    cd build
+    cd build || exit 1
     cmake -G "Ninja"  -DWITH_EEID=1 ..
     sudo ninja
     sudo ninja install
-    if [[ $? -eq 0 ]]; then
+    exit_code=$?
+    if [[ $exit_code -eq 0 ]]; then
         echo "OE SDK installed successfully"
     else
         echo "OE SDK install failed"
         exit 1
     fi
-    cd $home
+    cd "$home" || exit 1
 fi
 
 # Set log file that will be shared between two enclaves in the test scenario for stdout and stderr logs
 # In sgx-lkl test framework can have one log file for stdout and stderr. This log file is called with cat to be included in test's output
 # This file is also searched for some key sentences to verify TLS connection and MAA attestation results
-export MAA_TEST1_LOG_FILE=$SGXLKL_ROOT/remote_attestation_maa_test.log
-date > $MAA_TEST1_LOG_FILE
+export MAA_TEST1_LOG_FILE="$SGXLKL_ROOT/remote_attestation_maa_test.log"
+date > "$MAA_TEST1_LOG_FILE"
 
 # Kill hanging processes if exist
 process="sgx-lkl-run-oe"
@@ -70,24 +70,24 @@ if pgrep -x $process >/dev/null; then
 fi
 
 # Build and run OE enclave app
-cd oe_enclave
+cd oe_enclave || exit 1
 make
 make run
 
 # Build and run SGX-LKL enclave app
 cd ..
-cd sgxlkl_enclave
+cd sgxlkl_enclave || exit 1
 make lkl-image
 make lkl-run
 
 # Check the log file and decide if test passed
-maa_token=$(grep 'MAA JWT token obtained' $MAA_TEST1_LOG_FILE | wc -l)
-maa_parsed=$(grep 'Successfully parsed MAA JWT token' $MAA_TEST1_LOG_FILE | wc -l)
-maa_token_product_id=$(grep 'Successfully extracted product id from MAA JWT' $MAA_TEST1_LOG_FILE | wc -l)
-maa_token_oe_id=$(grep 'Successfuly populated OE identity from MAA JWT' $MAA_TEST1_LOG_FILE | wc -l)
-oe_identity_verified=$(grep 'OE identify verified successfully' $MAA_TEST1_LOG_FILE | wc -l)
+maa_token=$(grep -c 'MAA JWT token obtained' "$MAA_TEST1_LOG_FILE")
+maa_parsed=$(grep -c 'Successfully parsed MAA JWT token' "$MAA_TEST1_LOG_FILE")
+maa_token_product_id=$(grep -c 'Successfully extracted product id from MAA JWT' "$MAA_TEST1_LOG_FILE")
+maa_token_oe_id=$(grep -c 'Successfuly populated OE identity from MAA JWT' "$MAA_TEST1_LOG_FILE")
+oe_identity_verified=$(grep -c 'OE identify verified successfully' "$MAA_TEST1_LOG_FILE")
 
-cat $MAA_TEST1_LOG_FILE
+cat "$MAA_TEST1_LOG_FILE"
 [[ $maa_token -eq 1 && $maa_parsed -eq 1 && $maa_token_product_id -eq 1 && $maa_token_oe_id -eq 1 && $oe_identity_verified -eq 1 ]] && echo "TEST PASSED" && exit 0
 echo "TEST FAILED"
 exit 1
