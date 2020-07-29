f0c899da89840d2f4c4bd20ff6a5c5b4998b6c5e
Sean T. Allen <v-seall@microsoft.com>
2020-07-29 11:59:36 +0000

    Remove syscall-overrides work
    As I'm not using pread_fn, it no longer makes sense as part of this PR
    and would be part of the finishing of 604 once that is unblocked.

--- src/include/lkl/syscall-overrides-mem.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LKL_SYSCALL_OVERRIDES_MEM_H
-#define _LKL_SYSCALL_OVERRIDES_MEM_H
-
-/**
- * Register override functions for the memory management functions.
- */
-void syscall_register_mem_overrides();
-
-#endif
