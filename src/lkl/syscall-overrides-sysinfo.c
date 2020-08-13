a06e9b9c951756506b69eb67ea863a1cf77b64a2
Antonio Nino Diaz <anninodi@microsoft.com>
2020-08-13 10:32:31 +0000

    Report correct cpuinfo and sysinfo information
    For cpuinfo, the correct hook has been implemented in this commit.

--- src/lkl/syscall-overrides-sysinfo.c
+++ src/lkl/syscall-overrides-sysinfo.c
@@ -1,15 +1,19 @@
-#include "enclave/enclave_mem.h"
 #include <sys/sysinfo.h>
+
+#include "enclave/enclave_mem.h"
+#include "enclave/enclave_state.h"
 
 long syscall_sysinfo_override(struct sysinfo* info)
 {
+    const sgxlkl_enclave_config_t* econf = sgxlkl_enclave_state.config;
+
     size_t total, free;
     enclave_mem_info(&total, &free);
     info->totalram = total;
     info->freeram = free;
     info->totalswap = 0;
     info->freeswap = 0;
-    info->procs = 1;     // TODO: report # of ethreads
+    info->procs = econf->ethreads;
     info->totalhigh = 0;
     info->freehigh = 0;
     info->mem_unit = 1;
