b6e838e0034de86b48470b6a6bf87d2e262e65c9
bodzhang <zhanb@microsoft.com>
2021-04-12 13:33:53 -0400

    Design doc for securing the Virtio interface (#845)
    * Design doc for securing the Virtio interface
    
    Signed-off-by: Bo Zhang (ACC) <zhanb@microsoft.com>

--- src/lkl/virtio.c
+++ src/lkl/virtio.c
@@ -65,6 +65,25 @@ static inline uint32_t virtio_read_device_features(struct virtio_dev* dev)
  */
 static int virtio_read(void* data, int offset, void* res, int size)
 {
+     /* Security Review: use handle instead of virtio_dev pointer. The handle
+     * contains two virtio_dev pointers, one for the shadow structure in guest
+     * memory, one for the structure in host memory: struct virtio_dev_handle
+     * {
+     *     struct virtio_dev *dev; //shadow structure in guest memory
+     *     struct virtio_dev *dev_host;
+     * }
+     * dev->queue points to one or more virtq shadow structure. dev_host->queue
+     * points to virtq structure(s) in host memory. The desc ring and avail ring
+     * of the virtq shadow structure is also shadowed, as they should be
+     * host-read-only. The used ring of the virtq shadow structure points to the
+     * structure in host side directly, as it should be host-read-write and
+     * guest-read-only.
+     *
+     * For host-read-write members of a shadow structure, perform copy-through
+     * read (read from host structure & update the shadow structure). For
+     * host-write-once members, read from the shadow structure only, and the
+     * shadow structure init routine copy the content from the host structure.
+     */
     uint32_t val = 0;
     struct virtio_dev* dev = (struct virtio_dev*)data;
 
@@ -88,27 +107,36 @@ static int virtio_read(void* data, int offset, void* res, int size)
         case VIRTIO_MMIO_VERSION:
             val = VIRTIO_DEV_VERSION;
             break;
+        /* Security Review: dev->device_id should be host-write-once */
         case VIRTIO_MMIO_DEVICE_ID:
             val = dev->device_id;
             break;
+        /* Security Review: dev->device_id should be host-write-once */
         case VIRTIO_MMIO_VENDOR_ID:
             val = dev->vendor_id;
             break;
+        /* Security Review: dev->device_features should be host-write-once */
         case VIRTIO_MMIO_DEVICE_FEATURES:
             val = virtio_read_device_features(dev);
             break;
+        /* Security Review: dev->queue[dev->queue_sel].num_max should be
+         * host-write-once
+         */
         case VIRTIO_MMIO_QUEUE_NUM_MAX:
             val = dev->queue[dev->queue_sel].num_max;
             break;
         case VIRTIO_MMIO_QUEUE_READY:
             val = dev->queue[dev->queue_sel].ready;
             break;
+        /* Security Review: dev->int_status is host-read-write */
         case VIRTIO_MMIO_INTERRUPT_STATUS:
             val = dev->int_status;
             break;
+        /* Security Review: dev->status is host-read-write */
         case VIRTIO_MMIO_STATUS:
             val = dev->status;
             break;
+        /* Security Review: dev->config_gen should be host-write-once */
         case VIRTIO_MMIO_CONFIG_GENERATION:
             val = dev->config_gen;
             break;
@@ -211,7 +239,26 @@ static void virtio_notify_host_device(struct virtio_dev* dev, uint32_t qidx)
  */
 static int virtio_write(void* data, int offset, void* res, int size)
 {
+    /* Security Review: use handle instead of virtio_dev pointer. The handle
+     * contains two virtio_dev pointers, one for the shadow structure in guest
+     * memory, one for the structure in host memory: struct virtio_dev_handle
+     * {
+     *     struct virtio_dev *dev; //shadow structure in guest memory
+     *     struct virtio_dev *dev_host;
+     * }
+     * dev->queue points to one or more virtq shadow structure. dev_host->queue
+     * points to virtq structure(s) in host memory. The desc ring and avail ring
+     * of the virtq shadow structure is also shadowed, as they should be
+     * host-read-only. The used ring of the virtq shadow structure points to the
+     * structure in host side directly, as it should be host-read-write and
+     * guest-read-only.
+     *
+     * For host-read-only and host-read-write members of a shadow structure,
+     * perform copy-through write (write to shadow structure & to host
+     * structure). virtq desc and avail ring address handling is a special case.
+     */
     struct virtio_dev* dev = (struct virtio_dev*)data;
+    /* Security Review: dev->queue_sel should be host-read-only */
     struct virtq* q = &dev->queue[dev->queue_sel];
     uint32_t val;
     int ret = 0;
@@ -219,7 +266,9 @@ static int virtio_write(void* data, int offset, void* res, int size)
     if (offset >= VIRTIO_MMIO_CONFIG)
     {
         offset -= VIRTIO_MMIO_CONFIG;
-
+        /* Security Review: dev->config_data and dev->config_len should be
+         * host-write-once
+         */
         if (offset + size >= dev->config_len)
             return -LKL_EINVAL;
         memcpy(dev->config_data + offset, res, size);
@@ -234,49 +283,104 @@ static int virtio_write(void* data, int offset, void* res, int size)
 
     switch (offset)
     {
+        /* Security Review: dev->device_features_sel should be host-read-only */
         case VIRTIO_MMIO_DEVICE_FEATURES_SEL:
             if (val > 1)
                 return -LKL_EINVAL;
             dev->device_features_sel = val;
             break;
+        /* Security Review: dev->driver_features_sel should be host-read-only */
         case VIRTIO_MMIO_DRIVER_FEATURES_SEL:
             if (val > 1)
                 return -LKL_EINVAL;
             dev->driver_features_sel = val;
             break;
+        /* Security Review: dev->driver_features should be host-read-only */
         case VIRTIO_MMIO_DRIVER_FEATURES:
             virtio_write_driver_features(dev, val);
             break;
+        /* Security Review: dev->queue_sel should be host-read-only */
         case VIRTIO_MMIO_QUEUE_SEL:
             dev->queue_sel = val;
             break;
+        /* Security Review: dev->queue[dev->queue_sel].num should be
+         * host-read-only
+         */
         case VIRTIO_MMIO_QUEUE_NUM:
             dev->queue[dev->queue_sel].num = val;
             break;
+        /* Security Review: is dev->queue[dev->queue_sel].ready host-read-only?
+         */
         case VIRTIO_MMIO_QUEUE_READY:
             dev->queue[dev->queue_sel].ready = val;
             break;
+        /* Security Review: guest virtio driver(s) writes to virtq desc ring and
+         * avail ring in guest memory. In queue notify flow, we need to copy the
+         * update to desc ring and avail ring in host memory.
+         */
         case VIRTIO_MMIO_QUEUE_NOTIFY:
             virtio_notify_host_device(dev, val);
             break;
+        /* Security Review: dev->int_status is host-read-write */
         case VIRTIO_MMIO_INTERRUPT_ACK:
             dev->int_status = 0;
             break;
+        /* Security Review: dev->status is host-read-write */
         case VIRTIO_MMIO_STATUS:
             set_status(dev, val);
             break;
+        /* Security Review: For Split Queue, q->desc link list
+         * content should be host-read-only. The Split Queue implementaiton
+         * in guest side virtio code can be affected by host side manipulation
+         * of q->desc[].addr and q->desc[].next. Shadowing the desc link list
+         * requires extensive changes in the Split Queue code.
+         * 
+         * For Packed Queue, q->desc link list content is host-read-write, but
+         * the Packed Queue implementation in guest side virtio code does not
+         * read q->desc[].addr in buffer management flow, instead, maintaining
+         * a local copy of the info. So the host side manipulation of
+         * q->desc[].addr would not be effective. The Packed Queue
+         * implementation still reads q->desc[].length as the size of the data
+         * writtern to the "used" buffer, by the host side. Sanity check that
+         * q->desc[].length should not exceed buffer size allocated might still
+         * be required.
+         */
         case VIRTIO_MMIO_QUEUE_DESC_LOW:
             set_ptr_low((_Atomic(uint64_t)*)&q->desc, val);
             break;
         case VIRTIO_MMIO_QUEUE_DESC_HIGH:
             set_ptr_high((_Atomic(uint64_t)*)&q->desc, val);
             break;
+        /* Security Review: For Split Queue, q->avail link list content should be
+         * host-read-only. The Split Queue implementaiton
+         * in guest side virtio code only write to the avail link list, and does
+         * not read from it.As long as the implementaiton does not change, host
+         * side manipulation of the avail link list won't be effective. Shadowing
+         * the avail linked list has the same challenge as shadowing the desc link
+         * list.
+         * 
+         * For Packed Queue, "avail"/"driver" points to a 32-bit driver-to-device
+         * notification. The Packed Queue implementaiton in guest side only writes
+         * to it.
+         */
         case VIRTIO_MMIO_QUEUE_AVAIL_LOW:
             set_ptr_low((_Atomic(uint64_t)*)&q->avail, val);
             break;
         case VIRTIO_MMIO_QUEUE_AVAIL_HIGH:
             set_ptr_high((_Atomic(uint64_t)*)&q->avail, val);
             break;
+        /* Security Review: For Split Queue, q->used link list content should be
+         * guest-read-only. The Split Queue implementaiton in guest side virtio
+         * code directly read from the link list and has sanity check for
+         * unexpected value of q->used.idx and q->used[].id. It reads
+         * q->used[].length as the size of the data writtern to the "used"
+         * buffer, by the host side. Sanity check that q->used[].length should
+         * not exceed buffer size allocated might still be required.
+         * 
+         * For Packed Queue, "used"/"device" points to a 32-bit device-to-driver
+         * notification. Host side manipulation of "device" flag can only affect
+         * functionality.
+         */
         case VIRTIO_MMIO_QUEUE_USED_LOW:
             set_ptr_low((_Atomic(uint64_t)*)&q->used, val);
             break;
@@ -319,11 +423,23 @@ int lkl_virtio_dev_setup(
     if (dev->irq < 0)
         return 1;
 
+    /* Security Review: dev-vendor_id might cause overflow in
+     * virtio_deliver_irq[DEVICE_COUNT]
+     */
     virtio_deliver_irq[dev->vendor_id] = deliver_irq_cb;
+    /* Security Review: pass handle instead of virtio_dev pointer to the rest of
+     * the system. 
+     */
+    /* Security Review: shadow dev->base used in guest side only. No
+     * copy-through to the host side structure
+     */
     dev->base = register_iomem(dev, mmio_size, &virtio_ops);
 
     if (!lkl_is_running())
     {
+        /* Security Review: multi-thread invocation of this function can cause
+         * buffer overflow in lkl_virtio_devs[4096]
+         */
         avail = sizeof(lkl_virtio_devs) - (devs - lkl_virtio_devs);
         num_bytes = oe_snprintf(
             devs,
@@ -339,10 +455,12 @@ int lkl_virtio_dev_setup(
             return -LKL_ENOMEM;
         }
         devs += num_bytes;
+        /* Security Review: where is dev->virtio_mmio_id used? */
         dev->virtio_mmio_id = lkl_num_virtio_boot_devs++;
     }
     else
     {
+        /* Security Review: where is this function defined? */
         ret = lkl_sys_virtio_mmio_device_add(
             (long)dev->base, mmio_size, dev->irq);
         if (ret < 0)
@@ -350,6 +468,7 @@ int lkl_virtio_dev_setup(
             sgxlkl_error("Can't register mmio device\n");
             return -1;
         }
+        /* Security Review: where is dev->virtio_mmio_id used? */
         dev->virtio_mmio_id = lkl_num_virtio_boot_devs + ret;
     }
     return 0;
